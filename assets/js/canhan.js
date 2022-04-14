555d0fd8329eb06745b3b7c8572fdd40a0ac3607
NGHIA64 <83792258+NGHIA64@users.noreply.github.com>
2022-04-14 18:36:09 +0700

    ok

--- assets/js/canhan.js
+++ assets/js/canhan.js
@@ -1,3 +1,12 @@
-function thongtincanhan(){
+function thongtincanhan(){ 
+    var tb ;
+    var ten = localStorage.getItem('phiendangnhap');
+    var id = '<td>Tên đăng nhập</td><td>'+ten+'</td>'
+    var mk = '<td>Mật khẩu</td><td>'+localStorage.getItem(ten)+'</td>'
+    var dadoc = '<td>Khóa học</td><td></td>'
+    var level = '<td>Level</td><td></td>'
+    var doitt = '<td><button>Đổi thông tin</button></td><td></td>'
+    tb = '<tr>'+id+'</tr>'+'<tr>'+mk+'</tr>'+'<tr>'+dadoc+'</tr>'+'<tr>'+level+'</tr>'+'<tr>'+doitt+'</tr>'
+    return tb;
 
 }
